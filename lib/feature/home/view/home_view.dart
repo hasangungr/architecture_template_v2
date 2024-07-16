a9db1bf9ae60c762435f37d66b073054e45d34fc
Hasan Güngör <hasangungr1@gmail.com>
2024-07-16 14:48:11 +0300

    Folder Structure

---
+++ lib/feature/home/view/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:architecture_template_v2/feature/home/view/mixin/home_view_mixin.dart';
+
+part 'widget/home_appbar.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with HomeViewMixin {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: _HomeAppbar(),
+    );
+  }
+}
---
+++ lib/feature/home/view/widget/home_appbar.dart
@@ -0,0 +1,13 @@
+part of '../home_view.dart';
+
+class _HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppbar();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight); //?
+}
