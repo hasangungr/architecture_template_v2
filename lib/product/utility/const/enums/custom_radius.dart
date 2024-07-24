9b50e3cc8459af3bf001a6bdd4833c1fe39b7fa9
Hasan Güngör <hasangungr1@gmail.com>
2024-07-24 23:30:13 +0300

    Edit

---
+++ lib/product/utility/const/enums/custom_radius.dart
@@ -0,0 +1,8 @@
+enum CustomRadius {
+  small(8),
+  medium(16),
+  large(32);
+
+  final double value;
+  const CustomRadius(this.value);
+}
--- script/clean.sh
+++ script/clean.sh
@@ -13,4 +13,4 @@ cd ..;
 
 
 
-flutter pub get;
+flutter pub get;https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D1332085270168714%26id%3D1297390696971505%26set%3Da.1307506229293285&psig=AOvVaw3WiKqAC0vJg_xONS4mf6XR&ust=1721937345890000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMDxtta6wIcDFQAAAAAdAAAAABAE
